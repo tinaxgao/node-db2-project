960d812bd51e886548daa117b9c7461a4c8513ac
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-07 17:40:58 -0500

    seeds

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,39 @@
 // STRETCH
+const cars = [
+  {
+    vin: "111",
+    make: "Tesla",
+    model: "Model S",
+    mileage: 123,
+    title: "clean",
+    transmission: "automatic",
+  },
+  {
+    vin: "222",
+    make: "Tesla",
+    model: "Model 3",
+    mileage: 456,
+    title: "used",
+  },
+  {
+    vin: "333",
+    make: "Tesla",
+    model: "Model X",
+    mileage: 789,
+    title: "clean",
+    transmission: "manual",
+  },
+];
+
+// exports.seed = function (knex) {
+//   return knex("cars")
+//     .truncate()
+//     .then(() => {
+//       return knex("cars").insert(cars);
+//     });
+// };
+
+exports.seed = async function (knex) {
+  await knex("cars").truncate();
+  await knex("cars").insert(cars);
+};
