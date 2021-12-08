c1913b0c7e6e10f778359b3f0e86d1d83b28ddc3
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-07 21:47:34 -0500

    progress

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,34 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const Cars = require("./cars-model");
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const car = await Cars.getById(req.params.id);
+    if (!car) {
+      next({ status: 404, message: `car with id ${id} is not found` });
+    } else {
+      req.car = car;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  next()
+};
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  next()
+};
 
 const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  next()
+};
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+};
--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,19 @@
-const getAll = () => {
-  // DO YOUR MAGIC
-}
+const db = require("../../data/db-config");
 
-const getById = () => {
-  // DO YOUR MAGIC
-}
+const getAll = () => {
+  return db("cars");
+};
+
+const getById = (id) => {
+  return db("cars").where('id', id).first()
+};
 
 const create = () => {
   // DO YOUR MAGIC
-}
+};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+};
